704081d9cbd9bd8aac74825b72e8b37966391300
pabloGoros <pgorostiaga@itba.edu.ar>
2026-08-06 19:56:10 -0300

    feat: ui changes

--- src/components/Brand.tsx
+++ src/components/Brand.tsx
@@ -7,11 +7,12 @@ export function Brand({ onClick }: BrandProps) {
     <a className="brand" href="#home" onClick={onClick} aria-label="CB Travel Planner home">
       <img
         className="brand-logo"
-        src="/images/cb-travel-planner-logo.png"
-        alt="CB Travel Planner"
+        src="/images/cb-travel-planner-logo.svg"
+        alt=""
         width="669"
         height="660"
       />
+      <span className="brand-name">CB Travel Planner</span>
     </a>
   )
 }
